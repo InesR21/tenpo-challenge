8422396b782b94933fb391907a8309cca7241fc4
Ines Rojas <45639602+InesR21@users.noreply.github.com>
2023-01-13 10:56:09 -0300

    add data and img

---
+++ config/globalData.js
@@ -0,0 +1,86 @@
+export const carousel = [
+  {
+    title: "Tenpo",
+    subtitle: "Eats",
+    description: "DELIVER APP",
+  },
+  {
+    title: "Tenpo",
+    subtitle: "Eats",
+    description: "DELIVER APP",
+  },
+  {
+    title: "Tenpo",
+    subtitle: "Eats",
+    description: "DELIVER APP",
+  },
+];
+
+export const restaurants = [
+  {
+    name: "McDonalds",
+    score: "3.5",
+    time: "10-50 min.",
+    img: require("./../assets/images/restaurant/mcdonald.png"),
+    discount: "50% DCTO",
+    category: "Hamburgesas",
+  },
+  {
+    name: "MELT pizzas",
+    score: "4.5",
+    time: "10-60 min.",
+    img: require("./../assets/images/restaurant/melt.png"),
+    discount: "30% DCTO",
+    category: "Pizzeria - Snacks",
+  },
+  {
+    name: "YOKONO",
+    score: "3.5",
+    time: "10-50 min.",
+    img: require("./../assets/images/restaurant/yokono.png"),
+    discount: "20% DCTO",
+    category: "Snacks",
+  },
+  {
+    name: "Lasagna Pizza",
+    score: "3.5",
+    time: "10-50 min.",
+    img: require("./../assets/images/restaurant/papa.png"),
+    discount: "50% DCTO",
+    category: "Comida Italiana",
+  },
+];
+
+export const categories = [
+  {
+    name: "HAMBURGESAS",
+    img: require("./../assets/images/categories/burger.png"),
+  },
+  {
+    name: "ITALIANA",
+    img: require("./../assets/images/categories/pasta.png"),
+  },
+  {
+    name: "PIZZAS",
+    img: require("./../assets/images/categories/pizza.png"),
+  },
+];
+
+export const favorites = [
+  {
+    name: "McDonalds",
+    description: "Combo hambrugesa Bigmac",
+    score: "3.5",
+    time: "10-50 min.",
+    img: require("./../assets/img/favorites/bigmac.png"),
+    logo: require("./../assets/img/restaurant/mcdonalds.png"),
+  },
+  {
+    name: "MELT pizzas",
+    description: "Pizza Mediana 3 Ingredientes",
+    score: "3.5",
+    time: "10-50 min.",
+    img: require("./../assets/img/favorites/pizza.png"),
+    logo: require("./../assets/img/favorites/melt.png"),
+  },
+];
