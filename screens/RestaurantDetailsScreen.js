9a2758a349646ad36c536e7032498fc039973b56
Ines Rojas <45639602+InesR21@users.noreply.github.com>
2023-01-16 00:20:12 -0300

    Update RestaurantDetailsScreen.js

--- screens/RestaurantDetailsScreen.js
+++ screens/RestaurantDetailsScreen.js
@@ -1,13 +1,25 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Image } from "react-native";
 import React from "react";
 import { useRoute } from "@react-navigation/native";
+import { styled } from "nativewind";
+
+const Container = styled(View, "bg-white w-full h-full items-center ");
+const TextTitle = styled(Text, "text-xl font-bold mt-5");
+const TextDescription = styled(Text, "text-[#adadad] text-xs");
+const TextDetails = styled(Text, "text-[#adadad] text-xl mt-60");
+const ImageStyle = styled(Image, "w-36 h-36 mt-10 rounded-lg");
 
 const RestaurantDetailsScreen = () => {
-  const { discount, img, name, score, time } = useRoute()?.params?.restaurant;
+  const { img, name, category } = useRoute()?.params?.restaurant;
 
   return (
     <SafeAreaView>
-      <Text>{name}</Text>
+      <Container>
+        <ImageStyle source={img} />
+        <TextTitle>{name}</TextTitle>
+        <TextDescription>{category}</TextDescription>
+        <TextDetails>Detalle de restaurante</TextDetails>
+      </Container>
     </SafeAreaView>
   );
 };
