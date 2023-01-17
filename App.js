764559268de17b5a5cd7dc1783a85e1a552faa26
Ines Rojas <45639602+InesR21@users.noreply.github.com>
2023-01-17 20:04:29 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StyleSheet } from "react-native";
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
@@ -41,12 +40,3 @@ export default function App() {
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
