e2d1b84fbc6d61fe85f6f4dd42c326af3be0b7e1
Ines Rojas <45639602+InesR21@users.noreply.github.com>
2023-01-17 21:13:34 -0300

    add fonts

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import HomeScreen from "./screens/HomeScreen";
@@ -6,10 +6,41 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MapScreen from "./screens/MapScreen";
 import RestaurantDetailsScreen from "./screens/RestaurantDetailsScreen";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    RobotoBlack: require("./assets/fonts/Roboto-Black.ttf"),
+    RobotoRegular: require("./assets/fonts/Roboto-Regular.ttf"),
+    GothamBold: require("./assets/fonts/Gotham-Bold.otf"),
+    GothamBook: require("./assets/fonts/Gotham-Book.otf"),
+    GothamLight: require("./assets/fonts/Gotham-Light.otf"),
+    GothamMedium: require("./assets/fonts/Gotham-Medium.otf"),
+    GothamUltra: require("./assets/fonts/Gotham-Ultra.otf"),
+  });
+
+  async function hideSplashScreen() {
+    try {
+      await SplashScreen.hideAsync();
+    } catch (e) {
+      console.warn(e);
+    }
+  }
+
+  useEffect(() => {
+    if (!fontsLoaded) {
+      return;
+    }
+    hideSplashScreen();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
     <Provider store={store}>
       <NavigationContainer>
---
+++ utils/FontFamilys.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const fontStyles = StyleSheet.create({
+  RobotoBlack: {
+    fontFamily: "RobotoBlack",
+  },
+  RobotoRegular: {
+    fontFamily: "RobotoRegular",
+  },
+  GothamBold: {
+    fontFamily: "GothamBold",
+  },
+  GothamBook: {
+    fontFamily: "GothamBook",
+  },
+  GothamLight: {
+    fontFamily: "GothamLight",
+  },
+  GothamMedium: {
+    fontFamily: "GothamMedium",
+  },
+  GothamUltra: {
+    fontFamily: "GothamUltra",
+  },
+});
