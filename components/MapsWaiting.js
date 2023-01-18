8ed88714531eaf0f18c566dac0bd0bbd26ee24bf
Ines Rojas <45639602+InesR21@users.noreply.github.com>
2023-01-18 10:59:35 -0300

    Update MapsWaiting.js

--- components/MapsWaiting.js
+++ components/MapsWaiting.js
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { styled } from "nativewind";
+import { fontStyles } from "../utils/FontFamilys";
 
 const Container = styled(View, "bg-white w-full h-full items-center pt-[50%]");
 const TextStyle = styled(Text, "text-[#adadad] text-lg");
@@ -8,7 +9,9 @@ const TextStyle = styled(Text, "text-[#adadad] text-lg");
 const MapsWaiting = () => {
   return (
     <Container>
-      <TextStyle>Esperando tu ubicación...</TextStyle>
+      <TextStyle style={fontStyles.GothamLight}>
+        Esperando tu ubicación...
+      </TextStyle>
     </Container>
   );
 };
